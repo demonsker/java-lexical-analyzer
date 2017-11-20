981fbb1e26fcc2883b064aa491ba218ae0ec6174
Napat <paper-pat@hotmail.com>
2017-11-20 12:10:47 +0700

    initial push

---
+++ src/jflexdemo/Main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package jflexdemo;
+
+import java.io.FileReader;
+import java.io.IOException;
+/**
+ *
+ * @author comsci
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        Lexer lex = new Lexer(new FileReader("input.txt"));
+        lex.yylex();
+    }
+
+}
